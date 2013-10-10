53404113faa9ac1e52dc396685a28baae112c15a
AlexZabavsky <alex@uacoders.com>
2013-10-10 01:05:00 -0700

    Added menu items, created admin admin pages views placeholders

--- views/public.php
+++ views/public.php
@@ -1,15 +1,9 @@
 <?php
 /**
- * Represents the view for the public-facing component of the plugin.
+ * YADWS
  *
- * This typically includes any information, if any, that is rendered to the
- * frontend of the theme when the plugin is activated.
- *
- * @package   Plugin_Name
- * @author    Your Name <email@example.com>
+ * @package   YADWS
  * @license   GPL-2.0+
- * @link      http://example.com
- * @copyright 2013 Your Name or Company Name
+ * @link      https://github.com/AlexZabavsky/yadws
  */
 ?>
-<!-- This file is used to markup the public facing aspect of the plugin. -->
