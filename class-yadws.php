471089351211ab03f3b4393b59d966244e86181f
AlexZabavsky <alex@uacoders.com>
2013-10-09 20:50:36 -0700

    Keep adding parts of plugin skeleton: admin and public css and js files inclusion, admin views implementation, various boilerplate methods

--- class-yadws.php
+++ class-yadws.php
@@ -5,6 +5,8 @@
  * @package   YADWS
  * @license   GPL-2.0+
  * @link      https://github.com/AlexZabavsky/yadws
+ *
+ * Special thanks to creators of Wordpress Plugin Boilerplate - https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate
  */
 
 /**
@@ -26,20 +28,20 @@ class YADWS {
     protected $plugin_slug = 'yadws';
 
     /**
-      * Instance of this class.
-      * @var      object
-      */
+     * Instance of this class.
+     * @var      object
+     */
     protected static $instance = null;
 
     /**
-      * Slug of the plugin screen.
-      * @var      string
-      */
+     * Slug of the plugin screen.
+     * @var      string
+     */
     protected $plugin_screen_hook_suffix = null;
 
     /**
-      * Plugin intialization
-      */
+     * Plugin intialization
+     */
     private function __construct() {
 
         // Load plugin text domain
@@ -73,8 +75,89 @@ class YADWS {
     }
 
     /**
-      * Load the plugin text domain for translation.
-      */
+     * Return an instance of this class.
+     * @return    object    A single instance of this class.
+     */
+    public static function get_instance() {
+
+        // If the single instance hasn't been set, set it now.
+        if ( null == self::$instance ) {
+            self::$instance = new self;
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Fired when the plugin is activated.
+     * @param    boolean    $network_wide    True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog.
+     */
+    public static function activate( $network_wide ) {
+        if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+            if ( $network_wide  ) {
+                // Get all blog ids
+                $blog_ids = self::get_blog_ids();
+
+                foreach ( $blog_ids as $blog_id ) {
+                    switch_to_blog( $blog_id );
+                    self::single_activate();
+                }
+                restore_current_blog();
+            } else {
+                self::single_activate();
+            }
+        } else {
+            self::single_activate();
+        }
+    }
+
+    /**
+     * Fired when the plugin is deactivated.
+     * @param    boolean    $network_wide    True if WPMU superadmin uses "Network Deactivate" action, false if WPMU is disabled or plugin is deactivated on an individual blog.
+     */
+    public static function deactivate( $network_wide ) {
+        if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+            if ( $network_wide ) {
+                // Get all blog ids
+                $blog_ids = self::get_blog_ids();
+
+                foreach ( $blog_ids as $blog_id ) {
+                    switch_to_blog( $blog_id );
+                    self::single_deactivate();
+                }
+                restore_current_blog();
+            } else {
+                self::single_deactivate();
+            }
+        } else {
+            self::single_deactivate();
+        }
+    }
+
+    /**
+     * Get all blog ids of blogs in the current network that are:
+     * - not archived
+     * - not spam
+     * - not deleted
+     *
+     * @return  array|false The blog ids, false if no matches.
+     */
+    private static function get_blog_ids() {
+        global $wpdb;
+
+        // get an array of blog ids
+        $sql = "
+            SELECT blog_id FROM $wpdb->blogs
+            WHERE archived = '0' AND spam = '0'
+            AND deleted = '0'
+        ";
+        return $wpdb->get_col( $sql );
+    }
+    
+    
+    /**
+     * Load the plugin text domain for translation.
+     */
     public function load_plugin_textdomain() {
         $domain = $this->plugin_slug;
         $locale = apply_filters( 'plugin_locale', get_locale(), $domain );
@@ -84,9 +167,9 @@ class YADWS {
     }
 
     /**
-      * Fired when a new site is activated with a WPMU environment.
-      * @param	int	$blog_id ID of the new blog.
-      */
+     * Fired when a new site is activated with a WPMU environment.
+     * @param	int	$blog_id ID of the new blog.
+     */
     public function activate_new_site( $blog_id ) {
         if ( 1 !== did_action( 'wpmu_new_blog' ) )
             return;
@@ -97,15 +180,73 @@ class YADWS {
     }
 
     /**
-      * Fired for each blog when the plugin is activated.
-      */
+     * Fired for each blog when the plugin is activated.
+     */
     private static function single_activate() {
         // TODO: Define activation functionality here
     }
 
     /**
-    * Register the administration menu for this plugin into the WordPress Dashboard menu.
-    */
+     * Fired for each blog when the plugin is deactivated.
+     */
+    private static function single_deactivate() {
+      // TODO: Define deactivation functionality here
+    }
+
+    /**
+     * Register and enqueue admin-specific style sheet.
+     * @return    null    Return early if no settings page is registered.
+     */
+    public function enqueue_admin_styles() {
+        if ( ! isset( $this->plugin_screen_hook_suffix ) ) {
+            return;
+        }
+
+        $screen = get_current_screen();
+        if ( $screen->id == $this->plugin_screen_hook_suffix ) {
+            //wp_enqueue_style( $this->plugin_slug .'-admin-styles', plugins_url( 'css/admin.css', __FILE__ ), array(), self::VERSION );
+        }
+    }
+
+    /**
+     * Register and enqueue admin-specific JavaScript.
+     * @return    null    Return early if no settings page is registered.
+     */
+    public function enqueue_admin_scripts() {
+        if ( ! isset( $this->plugin_screen_hook_suffix ) ) {
+            return;
+        }
+
+        $screen = get_current_screen();
+        if ( $screen->id == $this->plugin_screen_hook_suffix ) {
+            //wp_enqueue_script( $this->plugin_slug . '-admin-script', plugins_url( 'js/admin.js', __FILE__ ), array( 'jquery' ), self::VERSION );
+        }
+    }
+
+    /**
+     * Register and enqueue public-facing style sheet.
+     */
+    public function enqueue_styles() {
+        //wp_enqueue_style( $this->plugin_slug . '-plugin-styles', plugins_url( 'css/public.css', __FILE__ ), array(), self::VERSION );
+    }
+
+    /**
+     * Register and enqueues public-facing JavaScript files.
+     */
+    public function enqueue_scripts() {
+        //wp_enqueue_script( $this->plugin_slug . '-plugin-script', plugins_url( 'js/public.js', __FILE__ ), array( 'jquery' ), self::VERSION );
+    }
+
+    /**
+     * Render the settings page for this plugin.
+     */
+    public function display_plugin_admin_page() {
+        //include_once( 'views/admin.php' );
+    }
+    
+    /**
+     * Register the administration menu for this plugin into the WordPress Dashboard menu.
+     */
     public function add_plugin_admin_menu() {
         /*
         * Add a settings page for this plugin to the Settings menu.
@@ -124,8 +265,8 @@ class YADWS {
     }
 
     /**
-    * Add settings action link to the plugins page.
-    */
+     * Add settings action link to the plugins page.
+     */
     public function add_action_links( $links ) {
         return array_merge(
             array(
